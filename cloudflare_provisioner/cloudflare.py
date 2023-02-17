d5da393344898724dff5292aa2c65213ec9e2e32
José Coelho <16445494+jcoelho93@users.noreply.github.com>
2023-02-17 14:40:47 +0000

    use aws secrets manager extension

--- cloudflare_provisioner/cloudflare.py
+++ cloudflare_provisioner/cloudflare.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 from requests import Response
 from cloudflare_provisioner.models import DnsRecord
@@ -11,8 +12,22 @@ class Cloudflare:
     """A Cloudflare API client
     """
     def __init__(self):
-        self.token = os.getenv('CLOUDFLARE_API_TOKEN')
+        self.token = self._get_api_token()
         self.headers = {'Authorization': f"Bearer {self.token}"}
+
+    def _get_api_token(self) -> str:
+        headers = {
+            "X-Aws-Parameters-Secrets-Token": os.environ.get('AWS_SESSION_TOKEN')
+        }
+        secretsmanager_extension_endpoint = "http://localhost:2773/secretsmanager/get?secretId="
+        secret_name = os.getenv('CLOUDFLARE_API_TOKEN_SECRET_NAME')
+
+        response = requests.get(
+            secretsmanager_extension_endpoint + secret_name,
+            headers=headers
+        )
+        secret_string = json.loads(response.text)['SecretString']
+        return secret_string
 
     def add_dns_record(self, record: DnsRecord) -> Response:
         """Adds a DNS record
